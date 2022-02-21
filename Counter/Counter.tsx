c10d789846e1e339d065d60fbf7b5bff68e70cf3
Denizhan Gonen <gonendenizhan@gmail.com>
2022-02-21 18:48:38 +0300

    initial commit

---
+++ Counter/Counter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface CounterProps {}
+
+const Counter: React.FC<CounterProps> = (props) => {
+  return <div>Counter Component</div>;
+};
+
+export default Counter;
