27cf25ac95d38b2370c534343c6d6ca64b1bba21
Denizhan Gonen <gonendenizhan@gmail.com>
2022-02-22 16:21:23 +0300

    delete

--- Counter/Counter.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface CounterProps {}
-
-const Counter: React.FC<CounterProps> = (props) => {
-  return <div>Counter Component</div>;
-};
-
-export default Counter;
